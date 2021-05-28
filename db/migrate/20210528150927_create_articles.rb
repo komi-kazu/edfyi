8140f6ef7637b85a9d94f67485fd6bb5f8986215
komi-kazu <yihuangxiaofeng@gmail.com>
2021-05-29 00:39:53 +0900

    投稿モデル、投稿コントローラー

---
+++ db/migrate/20210528150927_create_articles.rb
@@ -0,0 +1,12 @@
+class CreateArticles < ActiveRecord::Migration[6.0]
+  def change
+    create_table :articles do |t|
+      t.string :title, null: false
+      t.text :article, null: false
+      t.integer :tag, null: false
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
