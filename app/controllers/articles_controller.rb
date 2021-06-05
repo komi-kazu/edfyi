f3e0ec93031113d6495812c1bdfb173a02bf31e9
komi-kazu <yihuangxiaofeng@gmail.com>
2021-06-05 17:58:04 +0900

    投稿編集機能実装

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,9 +18,16 @@ class ArticlesController < ApplicationController
   end
 
   def edit
+    @article = Article.find(params[:id])
   end
 
   def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to article_path
+    else
+      render :edit
+    end
   end
 
   def show
