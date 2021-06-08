baba17736cf7ce1f96de6f9b681e87cf592e9d95
komi-kazu <yihuangxiaofeng@gmail.com>
2021-06-08 20:52:31 +0900

    良いね機能実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,10 +3,12 @@ class LikesController < ApplicationController
 
   def create
     Like.create(user_id: current_user.id, article_id: params[:id])
+    redirect_to article_path
   end
 
   def destroy
     Like.find_by(user_id: current_user.id, article_id: params[:id]).destroy
+    redirect_to article_path
   end
 
 private
