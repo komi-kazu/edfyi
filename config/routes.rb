f2661f3bb8a0d9332f6821138e64b8398766da0c
komi-kazu <yihuangxiaofeng@gmail.com>
2021-06-30 12:48:04 +0900

    dockerでtutorialルーティング成功

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,6 @@ Rails.application.routes.draw do
 
   post 'like/:id' => 'likes#create', as: 'create_like'
   delete 'like/:id' => 'likes#destroy', as: 'destroy_like'
+
+  get "/tutorial" => "articles#index"
 end
