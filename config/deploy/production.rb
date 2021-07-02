e6b5b81317e8d81d051d8ef5bfcfdead654028ab
komi-kazu <yihuangxiaofeng@gmail.com>
2021-07-02 13:45:41 +0900

    自動デプロイ導入

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -60,3 +60,9 @@
 #     # password: "please use keys"
 #   }
 server '35.75.50.222', user: 'ec2-user', roles: %w{app db web}
+
+set :ssh_options, {
+  keys: [ENV.fetch('PRODUCTION_SSH_KEY').to_s],
+  forward_agent: true,
+  auth_methods: %w[publickey]
+}
