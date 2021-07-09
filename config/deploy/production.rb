f5b0df07b2cec43d852ab3d5322cb9c9ecc4ebf5
komi-kazu <yihuangxiaofeng@gmail.com>
2021-07-09 15:59:56 +0900

    デプロイテスト３

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,10 +59,10 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-# server '35.75.50.222', user: 'ec2-user', roles: %w{app db web}
+server '35.75.50.222', user: 'ec2-user', roles: %w{app db web}
 
 set :ssh_options, {
-  keys: [ENV.fetch('PRODUCTION_SSH_KEY').to_s],
+  keys: ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCvj67l1ir+Wc6sl5yyiG5YVsJn0XU/u5MbDOPHOa4X+EQ1rhFKs6SSLXFMVcVH8UBss8Zues+wcP//I6FWCwxIee7JLjRm7kkau54/g2C15XmmBxJuMfJYc9gArxWTtT2EM0U3ZxMc0pTNjjbK8CQ9VY37bD6P7XI9U3g+yJyye5SCj/2JPTDqioVoy4/GZnO4+Qwkqg/m9WAQkOQ+YwWcsps0Z0NgM0G2r2u489LXt++buHrfsiWFzQlyNak8KlBAf6sl57ddy2jD1jcNVRsMlI0JPVQ7IzjpHuf/EYNnfz1XgwLxBmvHC4AU2s4vByZbzvdBxyZiyi4O8BLWQ/Q08YqSQ3skTGTfW9BQoPBgukB/c/jcMUyy8k5doASOYwD0asfDRDvQlQWAQdKc/dqAqBJROHNJOV2vTNxRs9SxS2K+w1i2l2RWmlVSygv4wURPDUA7GuR+Cy+y6Gb2vYrxAncXQBOCmmOwAdyeYtmnuoiHpPrRAYE7EgIWXmx7KJ6Fk+QGWznbQrM+a332AL0ocNDaPRBZ8SgH+WkB9vQT2h7l1T65lsU9Y6OLMCCcV6F4v0IFjLA3M3JfoLDt4NEGt49ppnN2suR0E5mdUKorjGwsH525i2pGcZsTC63fxATVtnrv88vzzNqtePs6gXO8s6SBHY/bkXrE1HYua2y9aQ== ec2-user@ip-10-0-0-151.ap-northeast-1.compute.internal,
   forward_agent: true,
   auth_methods: %w[publickey]
 }
